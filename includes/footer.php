800118d9752a71e460ba4790326cb83cf19da5e8
Robert de Grijs <rdgrijs@hotmail.com>
2020-07-02 10:31:54 +0200

    first draft

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+    <div id="footer">
+        <div class="container">
+            <p>&copy; 2020 Robert de Grijs. Alle rechten voorbehouden.</p>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="js/nivo-lightbox.js"></script>
+    <script type="text/javascript" src="js/jquery.isotope.js"></script>
+    <script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+    <script type="text/javascript" src="js/contact_me.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    </body>
+
+    </html>
---
+++ index.php
@@ -0,0 +1,268 @@
+<?php require "includes/header.php"; ?>
+
+<body>
+    <!-- Header -->
+    <header id="header">
+        <div class="intro">
+            <div class="overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-8">
+                            <div class="intro-text">
+                                <h1>Robert de Grijs</h1>
+                                <p id="intro_par">Web Developer</p>
+                                <a href="#portfolio" class="btn btn-custom btn-lg page-scroll">Portfolio</a> <a href="#contact" class="btn btn-custom btn-lg page-scroll">Over mij</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Portfolio Section -->
+    <div id="portfolio">
+        <div class="container">
+            <div class="section-title">
+                <h2>Portfolio</h2>
+            </div>
+            <div class="categories">
+                <ul class="cat">
+                    <li>
+                        <ol class="type">
+                            <li><a href="#" data-filter="*" class="active">All</a></li>
+                            <li><a href="#" data-filter=".lorem">Lorem Ipsum</a></li>
+                            <li><a href="#" data-filter=".dolor">Dolor Sit</a></li>
+                            <li><a href="#" data-filter=".adipiscing">Adipiscing Elit</a></li>
+                        </ol>
+                    </li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="portfolio-items">
+                    <div class="col-sm-6 col-md-4 lorem">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/01-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 adipiscing">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/02-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/02-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 lorem">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/03-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/03-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 lorem">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/04-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/04-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 adipiscing">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/05-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/05-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 dolor">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/06-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/06-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 dolor">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/07-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/07-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 lorem">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/08-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/08-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 adipiscing">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/09-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/09-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 adipiscing">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/10-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/10-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 adipiscing">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/11-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/11-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 lorem">
+                        <div class="portfolio-item">
+                            <div class="hover-bg"> <a href="img/portfolio/12-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div class="hover-text">
+                                        <div class="overlay-caption">
+                                            <div class="overlay-content">
+                                                <h4>Project Name</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="img/portfolio/12-small.jpg" class="img-responsive" alt="Project Title">
+                                </a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact Section -->
+    <div id="contact">
+        <div class="container">
+            <h2>Over mij</h2>
+            <div class="col-md-6">
+                <div class="section-title">
+                    <div class="row"> <img src="img/robert.jpg" class="img-responsive pull-left" alt="">
+                        <p>
+                            Ik ben sinds kort een nieuw pad ingeslagen als web developer. Tijdens de bootcamp bij CodeGorilla heb ik veel kennis opgedaan van HTML, CSS en JavaScript. In mijn rol als Tech Coach bij CodeGorilla heb ik voortgebouwd op deze kennis en deze vergroot. Daarnaast heb ik mijn communicatieve vaardigheden verbreed bij het begeleiden van aanstormend programmeertalent.
+                        </p>
+                        <p>
+                            Door ervaringen bij vorige banen neem ik oog voor detail en efficiëntie mee. Deze eigenschappen pas ik graag toe bij het schrijven van code. Ik houd ervan om de werksfeer open en gezellig te houden. Ik ben communicatief vaardig en stel mijzelf altijd professioneel op. Mijn ideale toekomstplaatje ziet er als volgt uit: werken als developer bij een IT bedrijf met leuke collega's waar ik de ruimte krijg om mijn kennis uit te breiden om zo een meerwaarde te vormen voor mijn collega's en werkgever.
+                        </p>
+
+                        <div class="social">
+                            <ul>
+                                <li><a href="https://www.linkedin.com/in/robert-de-grijs-06775140/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="https://github.com/RDG83" target="_blank"><i class="fa fa-github"></i></a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 col-md-offset-1">
+                <div class="section-title">
+                    <p>Voor vragen kunt u contact met mij opnemen via het mailadres <strong>info@company.com</strong> of via het onderstaande formulier.</p>
+                </div>
+                <form name="sentMessage" id="contactForm" novalidate>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" id="name" class="form-control" placeholder="Naam" required="required">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="message" id="message" class="form-control" rows="4" placeholder="Bericht" required></textarea>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div id="success"></div>
+                    <button type="submit" class="btn btn-default btn-lg">Verzend</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php require "includes/footer.php"; ?>
